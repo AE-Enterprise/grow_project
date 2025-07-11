bb54462f1dd0201d1451fa60dbe6e9b248ef1c67
Alex Elwell <alexander.hayden.elwell@gmail.com>
2025-07-11 18:50:47 +0100

    fix: added new micro services for 3 projcs

---
+++ src/API/Plants/inventory/models.py
@@ -0,0 +1,14 @@
+'''Plan Inventory Models'''
+
+from django.db import models
+
+# Create your models here.
+class Plant(models.Model):
+    '''Model representing a plant in the inventory.'''
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    date_planted = models.DateField()
+    location = models.CharField(max_length=100, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return f"{self.name} ({self.species})"
