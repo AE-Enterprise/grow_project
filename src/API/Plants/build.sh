e73ab361d9ca04e51c6914c77230a37b3e4a061b
Alex Elwell <alexander.hayden.elwell@gmail.com>
2025-07-13 18:12:48 +0100

    feat: added dev helm for local development DB connection

--- src/API/Plants/build.sh
+++ src/API/Plants/build.sh
@@ -1,2 +1,1 @@
-# Build and push the plants image to k8s.io namespace for Rancher Desktop
-nerdctl --namespace k8s.io build -t plants:latest .
+nerdctl --namespace k8s.io build -t plants:latest-dev -f Dockerfile.dev .
